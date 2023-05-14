af10bace7048fe3fff8795627450d2f5392a3b3a
cy <chenyou821@outlook.com>
2023-05-14 15:20:09 +0800

    netty_client_test

---
+++ src/main/java/org/example/APP.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Hello world!
+ */
+public class APP {
+    public static void main(String[] args) {
+        MyClientHandler myClientHandler = new MyClientHandler();
+        NioEventLoopGroup eventExecutors = new NioEventLoopGroup();
+        try {
+            //创建bootstrap对象，配置参数
+            Bootstrap bootstrap = new Bootstrap();
+            //设置线程组
+            bootstrap.group(eventExecutors)
+                    //设置客户端的通道实现类型
+                    .channel(NioSocketChannel.class)
+                    //使用匿名内部类初始化通道
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //添加客户端通道的处理器
+                            ch.pipeline().addLast(myClientHandler);
+                        }
+                    });
+            System.out.println("客户端准备就绪，随时可以起飞~");
+            //连接服务端
+            ChannelFuture channelFuture = bootstrap.connect("8.130.42.107", 2181).sync();
+
+            Channel channel = channelFuture.channel();
+
+            for(int i = 0; i < 10; i++) {
+//                channel.writeAndFlush(Unpooled.copiedBuffer("dh236发给服务端循环信息中的第" + i + "条消息。", CharsetUtil.UTF_8));
+                myClientHandler.sendMessage(Unpooled.copiedBuffer("dh236发给服务端循环信息中的第" + i + "条消息。", CharsetUtil.UTF_8));
+                myClientHandler.waitForResponse();
+            }
+
+            //对通道关闭进行监听
+            channel.closeFuture().sync();
+        } catch(InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //关闭线程组
+            eventExecutors.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/org/example/MyClientHandler.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class MyClientHandler extends ChannelInboundHandlerAdapter {
+
+    private ChannelHandlerContext ctx;
+
+//    控制等待回应的操作
+    private CountDownLatch latch;
+
+    public void sendMessage(Object message) {
+//        发送消息的时候，将计数器的设置为1
+        latch = new CountDownLatch(1);
+        ctx.writeAndFlush(message);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        //发送消息到服务端
+//        ctx.writeAndFlush(Unpooled.copiedBuffer("歪比巴卜~茉莉~Are you good~马来西亚~：dh自动发送的第一条消息。", CharsetUtil.UTF_8));
+        this.ctx = ctx;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //接收服务端发送过来的消息
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println("收到服务端" + ctx.channel().remoteAddress() + "的消息：" + byteBuf.toString(CharsetUtil.UTF_8) + "\n");
+//        收到服务器的消息的时候，将计数器减1
+        latch.countDown();
+    }
+
+    public void waitForResponse() throws InterruptedException {
+//        在等待回应的操作中，调用await方法进行等待，直到计数器为0，即可收到服务端的回应
+        boolean success = latch.await(1, TimeUnit.SECONDS);
+        if (!success) {
+            System.out.println("丢失了服务器的返回的一条消息！！！ \n");
+        }
+    }
+}
